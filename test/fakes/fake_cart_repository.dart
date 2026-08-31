d22b1a82471425c52078163967cd39dfb68b35d4
Sachin Shukla <sachinkiet@gmail.com>
2026-09-01 00:07:07 +0530

    [MA-123] [App] feat(cart): CartRepository.getCart/updateItem/removeItem
    Extends the existing CartRepository (addItem only, from MA-120) with
    MA-123's three additional Cart Service endpoints, plus a matching fake.

--- test/fakes/fake_cart_repository.dart
+++ test/fakes/fake_cart_repository.dart
@@ -1,4 +1,6 @@
 import 'package:milkful_app/features/cart/data/cart_repository.dart';
+import 'package:milkful_app/features/cart/models/cart_line_item.dart';
+import 'package:milkful_app/features/cart/models/cart_view.dart';
 import 'package:milkful_app/features/cart/models/frequency.dart';
 
 class FakeAddItemRequest {
@@ -17,12 +19,34 @@ class FakeAddItemRequest {
   final DateTime? startDate;
 }
 
+class FakeUpdateItemRequest {
+  FakeUpdateItemRequest({required this.items, required this.ifVersion});
+
+  final List<CartLineItem> items;
+  final int ifVersion;
+}
+
 class FakeCartRepository implements CartRepository {
-  FakeCartRepository({this.addItemException});
+  FakeCartRepository({
+    this.addItemException,
+    this.getCartResult,
+    this.getCartException,
+    this.updateItemResult,
+    this.updateItemException,
+    this.removeItemException,
+  });
 
   Object? addItemException;
+  CartView? getCartResult;
+  Object? getCartException;
+  CartView? updateItemResult;
+  Object? updateItemException;
+  Object? removeItemException;
 
   final List<FakeAddItemRequest> requests = [];
+  final List<FakeUpdateItemRequest> updateItemRequests = [];
+  final List<String> removeItemRequests = [];
+  int getCartCallCount = 0;
 
   @override
   Future<void> addItem({
@@ -42,5 +66,28 @@ class FakeCartRepository implements CartRepository {
       ),
     );
     if (addItemException != null) throw addItemException!;
+  }
+
+  @override
+  Future<CartView> getCart() async {
+    getCartCallCount++;
+    if (getCartException != null) throw getCartException!;
+    return getCartResult ?? const CartView(items: [], cartVersion: 0);
+  }
+
+  @override
+  Future<CartView> updateItem({
+    required List<CartLineItem> items,
+    required int ifVersion,
+  }) async {
+    updateItemRequests.add(FakeUpdateItemRequest(items: items, ifVersion: ifVersion));
+    if (updateItemException != null) throw updateItemException!;
+    return updateItemResult ?? CartView(items: items, cartVersion: ifVersion + 1);
+  }
+
+  @override
+  Future<void> removeItem({required String id}) async {
+    removeItemRequests.add(id);
+    if (removeItemException != null) throw removeItemException!;
   }
 }
